0bb505ada956fb7f45f95bbd319255362cef3cb4
Nastya <nastya_mi@icloud.com>
2019-12-22 20:13:51 +0300

    feat: 20 task solved

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -180,8 +180,11 @@ function getParallelipidedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  if (pow === 0) {
+    return num;
+  }
+  return Math.round(num / (10 ** pow)) * (10 ** pow);
 }
 
 /**
